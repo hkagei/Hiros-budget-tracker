63dc929a93e4b343d91dd2a46521b7796e333533
hkagei <k_dawg_1022@hotmail.com>
2022-07-17 20:59:59 -0600

    preliminary completion

--- public/js/idb.js
+++ public/js/idb.js
@@ -13,7 +13,7 @@ request.onsuccess = function(event) {
   
     // check if app is online, if yes run checkDatabase() function to send all local db data to api
     if (navigator.onLine) {
-    //   uploadData();
+    uploadData();
     }
   };
 
@@ -22,4 +22,60 @@ request.onsuccess = function(event) {
     console.log(event.target.errorCode);
   };
 
+  function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_data'], 'readwrite');
   
+    // access the object store for `new_data`
+    const dataObjectStore = transaction.objectStore('new_data');
+  
+    // add record to your store with add method
+    dataObjectStore.add(record);
+  }
+
+  function uploadData() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_data'], 'readwrite');
+  
+    // access your object store
+    const dataObjectStore = transaction.objectStore('new_data');
+  
+    // get all records from store and set to a variable
+    const getAll = dataObjectStore.getAll();
+  
+    // more to come...
+    // upon a successful .getAll() execution, run this function
+getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/routes/api/', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_data'], 'readwrite');
+          // access the new_data object store
+          const dataObjectStore = transaction.objectStore('new_data');
+          // clear all items in your store
+          dataObjectStore.clear();
+
+          alert('All saved data has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+  }
+
+  // listen for app coming back online
+window.addEventListener('online', uploadData);
